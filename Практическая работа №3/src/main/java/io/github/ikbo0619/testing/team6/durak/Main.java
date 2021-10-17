8ed2322931f5bbdec9aff2226adf9881e547a089
tuguzT <timurka.tugushev@gmail.com>
2021-10-17 13:24:08 +0300

    Initial commit for Practice 3

---
+++ Практическая работа №3/src/main/java/io/github/ikbo0619/testing/team6/durak/Main.java
@@ -0,0 +1,7 @@
+package io.github.ikbo0619.testing.team6.durak;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ Практическая работа №3/src/test/java/io/github/ikbo0619/testing/team6/durak/FirstTest.java
@@ -0,0 +1,8 @@
+package io.github.ikbo0619.testing.team6.durak;
+
+import org.junit.jupiter.api.Test;
+
+public class FirstTest {
+    @Test
+    public void emptyTest() {}
+}
