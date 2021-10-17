d16ff77c6605f5c36f6a2728965b916b71797600
tuguzT <timurka.tugushev@gmail.com>
2021-10-17 19:18:43 +0300

    First set of tests passed

--- Практическая работа №3/src/main/java/io/github/ikbo0619/testing/team6/durak/Field.java
+++ Практическая работа №3/src/main/java/io/github/ikbo0619/testing/team6/durak/Field.java
@@ -3,13 +3,25 @@ package io.github.ikbo0619.testing.team6.durak;
 import java.util.ArrayList;
 
 public class Field {
-    private ArrayList<Pair> pairs;
-    private ArrayList<String> playedRanks;
+    private final ArrayList<Pair> pairs;
+    private final ArrayList<String> playedRanks;
     private boolean completed;
 
-    public Field() {}
+    public Field() {
+        pairs = new ArrayList<>();
+        playedRanks = new ArrayList<>();
+        completed = false;
+    }
 
-    public Field(Card attackCard) {}
+    public Field(Card attackCard) {
+        this();
+
+        Pair initialPair = new Pair(attackCard);
+        pairs.add(initialPair);
+
+        String initialCardRank = attackCard.getRank();
+        playedRanks.add(initialCardRank);
+    }
 
     public void attack(Card attackCard) {}
 
@@ -28,21 +40,28 @@ public class Field {
     }
 
     public boolean isCompleted() {
-        return false;
+        return completed;
     }
 
     public Pair currentOpenPair() {
         return null;
     }
 
-    public void toggleCompleted() {}
+    public void toggleCompleted() {
+        completed = !completed;
+    }
 
     public boolean endField() {
         return false;
     }
 
     public ArrayList<Card> fetchAllCards() {
-        return null;
+        ArrayList<Card> result = new ArrayList<>();
+        for (Pair pair : pairs) {
+            ArrayList<Card> pairCards = pair.fetchAllCards();
+            result.addAll(pairCards);
+        }
+        return result;
     }
 
     @Override
